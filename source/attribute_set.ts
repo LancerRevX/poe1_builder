6cf43622cf0ce163199cd802f047d2492da630fd
LancerRevX <50010225+LancerRevX@users.noreply.github.com>
2021-05-18 11:05:01 +0700

    delete all files

--- source/attribute_set.ts
+++
@@ -1,5 +0,0 @@
-enum Attribute {
-    MIGHT, CONSTITUTION, DEXTERITY, PERCEPTION, INTELLECT, RESOLVE
-}
-
-type AttributeSet = [number, number, number, number, number, number]
--- source/skill_set.ts
+++
@@ -1,5 +0,0 @@
-enum Skill {
-    STEALTH, ATHLETICS, LORE, MECHANICS, SURVIVAL
-}
-
-type SkillSet = [number, number, number, number, number]
