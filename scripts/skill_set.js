a7d4a528535e5cdae31399cb815a3ab2447a866e
LancerRevX <50010225+LancerRevX@users.noreply.github.com>
2021-05-18 14:33:08 +0700

    delete all files

--- scripts/skill_set.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Skill;
-(function (Skill) {
-    Skill[Skill["STEALTH"] = 0] = "STEALTH";
-    Skill[Skill["ATHLETICS"] = 1] = "ATHLETICS";
-    Skill[Skill["LORE"] = 2] = "LORE";
-    Skill[Skill["MECHANICS"] = 3] = "MECHANICS";
-    Skill[Skill["SURVIVAL"] = 4] = "SURVIVAL";
-})(Skill || (Skill = {}));
